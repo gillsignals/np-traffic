b5ca04982e28e46b8029bf09272433355ca3385b
Amy Gill <gill.signals@gmail.com>
2021-10-26 11:43:21 -0400

    Add my colorblind color template.

---
+++ colorblind_colors.m
@@ -0,0 +1,15 @@
+%% DEFINE COLORBLIND FRIENDLY PLOT COLORS
+% To use a certain color, use `colors(cb.colorname)` with desired color name
+
+% assign vector elements to color names
+cb.black = 1;
+cb.skyblue = 2;
+cb.orange = 3;
+cb.green = 4;
+cb.red = 5;
+cb.darkblue = 6;
+cb.yellow = 7;
+cb.pink = 8;
+
+% assign colors in order above (black, skyblue, orange, green, red, darkblue, yellow, pink
+colors = ["#000000", "#56B4E9", "#E69F00", "#009E73", "#D55E00", "#0072B2", "#F0E442", "#CC79A7"];
